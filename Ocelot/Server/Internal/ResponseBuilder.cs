71cbd2a440e7f765bdc1ea0c87754513eca90af4
Navid M <navidmomtahen@gmail.com>
2024-08-28 00:36:31 +0100

    fmt

--- Ocelot/Server/Internal/ResponseBuilder.cs
+++ Ocelot/Server/Internal/ResponseBuilder.cs
@@ -6,10 +6,12 @@ internal static class ResponseBuilder
 {
     public static byte[] GenerateHttpResponse(Response response)
     {
-        var content = response.GetContent();
-        var headers = System.Text.Encoding.UTF8.GetBytes(
-            $"HTTP/1.1 200 OK\r\nContent-Type: {response.ContentType}\r\nContent-Length: {content.Length}\r\nConnection: close\r\n\r\n"
+        byte[] content = response.GetContent();
+        return ContentWriter.CombineHeadersAndResponse(
+            System.Text.Encoding.UTF8.GetBytes(
+                $"HTTP/1.1 200 OK\r\nContent-Type: {response.ContentType}\r\nContent-Length: {content.Length}\r\nConnection: close\r\n\r\n"
+            ),
+            content
         );
-        return ContentWriter.CombineHeadersAndResponse(headers, content);
     }
 }
