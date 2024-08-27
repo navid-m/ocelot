8f988b93e7f26048b7a411b3a174067774e2693a
Navid M <navidmomtahen@gmail.com>
2024-08-28 00:37:43 +0100

    type

--- Ocelot/Server/Internal/ContentWriter.cs
+++ Ocelot/Server/Internal/ContentWriter.cs
@@ -4,7 +4,7 @@ internal static class ContentWriter
 {
     public static byte[] CombineHeadersAndResponse(byte[] headers, byte[] response)
     {
-        var result = new byte[headers.Length + response.Length];
+        byte[] result = new byte[headers.Length + response.Length];
         Buffer.BlockCopy(headers, 0, result, 0, headers.Length);
         Buffer.BlockCopy(response, 0, result, headers.Length, response.Length);
         return result;
