f9c3747a1c3198733188c48474a93402d23d3037
Navid M <navidmomtahen@gmail.com>
2024-08-27 02:31:52 +0100

    rename

--- Ocelot/Program.cs
+++ Ocelot/Program.cs
@@ -5,8 +5,8 @@ class Program
 {
     static async Task Main(string[] args)
     {
-        var framework = new HTTPServer("127.0.0.1", 5001);
-        framework.RegisterRoutes<ExampleController>();
-        await framework.StartAsync();
+        var app = new HTTPServer("127.0.0.1", 5001);
+        app.RegisterRoutes<ExampleController>();
+        await app.StartAsync();
     }
 }
