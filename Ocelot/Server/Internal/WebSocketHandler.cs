d695e4b915454df757cc9b9abe9111fdeaff5fd7
Navid M <navidmomtahen@gmail.com>
2024-08-29 16:27:37 +0100

    remove and fmt

--- Ocelot/Server/Internal/WebSocketHandler.cs
+++ Ocelot/Server/Internal/WebSocketHandler.cs
@@ -2,19 +2,18 @@ using System.Net.WebSockets;
 using System.Reflection;
 using System.Text.RegularExpressions;
 
-namespace Ocelot.Server.Internal
+namespace Ocelot.Server.Internal;
+
+internal sealed class WebSocketHandler(string routePattern, MethodInfo method, object instance)
 {
-    internal sealed class WebSocketHandler(string routePattern, MethodInfo method, object instance)
+    private readonly MethodInfo _method = method;
+    private readonly object _instance = instance;
+    private readonly Regex _routeRegex = new($"^{routePattern}$", RegexOptions.Compiled);
+
+    public bool IsMatch(string route) => _routeRegex.IsMatch(route);
+
+    public async Task HandleWebSocketAsync(WebSocket webSocket)
     {
-        private readonly MethodInfo _method = method;
-        private readonly object _instance = instance;
-        private readonly Regex _routeRegex = new($"^{routePattern}$", RegexOptions.Compiled);
-
-        public bool IsMatch(string route) => _routeRegex.IsMatch(route);
-
-        public async Task HandleWebSocketAsync(WebSocket webSocket)
-        {
-            await (Task)_method.Invoke(_instance, [webSocket])!;
-        }
+        await (Task)_method.Invoke(_instance, [webSocket])!;
     }
 }
--- Ocelot/Server/Middleware/StaticFileMiddleware.cs
+++ Ocelot/Server/Middleware/StaticFileMiddleware.cs
@@ -1,7 +1,4 @@
-using System.Diagnostics.CodeAnalysis;
 using System.Net;
-using System.Text;
-using Ocelot.Server.Internal;
 
 namespace Ocelot.Server.Middleware;
 
